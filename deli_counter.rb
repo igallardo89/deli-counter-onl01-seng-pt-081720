8fda482d1dd8bcc43ca5fad2899168594cd43819
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-25 20:14:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 katz_deli = [ ]
 
-def line(katz_deli)
-  count = 0 
-    if count == 0 
-      return "The line is currently empty!"
+def line(katz_deli = "The line is currently empty")
+      
+      
+      
   
   
 end
