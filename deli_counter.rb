59c6bfbc8d6d256d19910ad38c7f8058e67c8766
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-25 20:25:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,8 @@ katz_deli = [ ]
 def line(queue)
   customers = [ ]
   if queue.lenght >=1 
-  
-  
-
+    queue.each_with_index{|name,index| customers.push("#{index}.#{name}")
+    
 else
   "The line is currently empty."
   
